ca37facd031b21d7cc653cf371fcb1abab401d17
ashwinjadhav818 <ashwinjadhav818@gmail.com>
2024-03-21 17:38:10 +0530

    v2.2.3 - Various bug fixes and overall improvement

---
+++ themePreview.ps1
@@ -0,0 +1,3 @@
+mv -Force .\obsidian-aura\src\theme.css .\obsidian-aura\theme.css
+mv -Force .\obsidian-aura\src\theme.css.map .\obsidian-aura\theme.css.map
+cp .\obsidian-aura\theme.css '.\Aura Test Vault\.obsidian\themes\Aura\theme.css'
