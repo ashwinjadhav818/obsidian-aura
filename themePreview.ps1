b3c806100d8b9de6c321786c30efaf39dfe6282c
ashwinjadhav818 <ashwinjadhav818@gmail.com>
2024-03-24 14:44:59 +0530

    fix: workspace background glitches in card layout

--- themePreview.ps1
+++ themePreview.ps1
@@ -1,3 +1,2 @@
-mv -Force .\obsidian-aura\src\theme.css .\obsidian-aura\theme.css
-mv -Force .\obsidian-aura\src\theme.css.map .\obsidian-aura\theme.css.map
+sass .\obsidian-aura\src\theme.scss .\obsidian-aura\theme.css
 cp .\obsidian-aura\theme.css '.\Aura Test Vault\.obsidian\themes\Aura\theme.css'
